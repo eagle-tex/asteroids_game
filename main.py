1ad2783f576c929d864e8f8cd26de9a5d13097bd
Gildas Montcho <gmontcho@gmail.com>
2025-01-27 00:20:37 +0100

    feat: draw player

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -15,11 +16,14 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
 
         # pause the game for 1/60th of a second
